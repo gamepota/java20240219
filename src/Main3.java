27d45dc8c2ddfb7d364d8110de28e682532633c5
Dongwon <kdw686968@gmail.com>
2024-02-19 12:41:00 +0900

    처음 만든 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("그거 아시나요?");
+        System.out.println("레몬 한개에는");
+        System.out.println("무려 레몬 4개분량의");
+        System.out.println("비타민C가 있다는걸요.");
+        System.out.println("그럼 이만");
+
+
+
+    }
+}

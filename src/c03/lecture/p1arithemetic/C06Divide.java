43547899d708988ac4a09c5d5dfb8b854aa7eaaf
Dongwon <kdw686968@gmail.com>
2024-02-22 15:49:40 +0900

    2/22 학습내용

---
+++ src/c03/lecture/p1arithemetic/C06Divide.java
@@ -0,0 +1,49 @@
+package c03.lecture.p1arithemetic;
+
+public class C06Divide {
+    public static void main(String[] args) {
+        // 실수(double)를 0으로 나누면 무한대가 된다.
+
+        double a = 3.0;
+        double b = 0.0;
+        double c = a / b;
+        double d = (-a) / b;
+
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+
+        double e = 0.0;
+        double f = -0.0;
+
+        double g = a / e;// limit 개념
+        double h = a / f;
+
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+
+        //실수에서 나머지(%)연산 시 NaN (NOT a NUMBER)
+        double i = 3.0;
+        double j = 0.0;
+        double k = a % b;
+        System.out.println("k = " + k);
+
+        //Infinity 인지
+        boolean l = Double.isInfinite(h);
+        System.out.println("l = " + l);
+
+        //NaN 인지
+
+        boolean m = Double.isInfinite(k);
+        System.out.println("m = " + m);
+
+        //infinuty 와 연산결과는 infinity or NaN
+        double n = g - g;
+        System.out.println("n = " + n);
+        double o = g - 1000000000000000000000000000.0;
+        System.out.println("o = " + o);
+        //NaN과 연산 결과는 NaN
+        double p = n * 3.0;
+        System.out.println("p = " + p);
+
+    }
+}
---
+++ src/c03/sec3/CompareOperationExample.java
@@ -0,0 +1,55 @@
+package c03.sec3;
+
+public class CompareOperationExample {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 10;
+        boolean result1 = (num1 == num2);
+        boolean result2 = (num1 != num2);
+        boolean result3 = (num1 <= num2);
+        System.out.println("result1: " + result1);
+        System.out.println("result2: " + result2);
+        System.out.println("result3: " + result3);
+
+        char char1 = 'A';
+        char char2 = 'B';
+        boolean result4 = (char1 < char2);
+        System.out.println("result4: " + result4);
+        // ctrl + w 단어 덩어리 지정
+
+        int num3 = 1;
+        double num4 = 1.0;
+        boolean result5 = (num3 == num4);
+        System.out.println("result5: " + result5);
+
+        float num5 = 0.1f;
+        double num6 = 0.1;
+        boolean result6 = (num5 == num6);
+        boolean result7 = (num5 == (float) num6);
+        // 실수간의 ==, != 연산은 매우 주의
+
+        System.out.println("result6: " + result6);
+        System.out.println("result7: " + result7);
+
+        //String 간의 비교는 equals 메소드 사용
+
+        String str1 = "자바";
+        String str2 = "Java";
+        boolean result8 = (str1.equals(str2));
+        boolean result9 = (!str1.equals(str2));
+        System.out.println("result8: " + result8);
+        System.out.println("result9: " + result9);
+
+        String s = "자";
+        String t = "바";
+        String u = s + t;
+        System.out.println("u = " + u);
+
+        boolean b1 = (str1 == u);
+        System.out.println("b1 = " + b1);
+
+        boolean b2 = str1.equals(u);
+        System.out.println("b2 = " + b2);
+
+    }
+}
